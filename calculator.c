435b216f704dce0bb2939da09fe55a13d32c3ffc
Live session user <pedrogomesyoo@gmail.com>
2015-05-29 09:40:36 +0000

    Base

---
+++ calculator.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h> // for getch
+ 
+main() {
+       float OP1, OP2;
+       char caracter;
+       
+       printf("Introduce first operator:");
+       scanf("%f", &OP1);
+       printf("Introduce second operator:");
+       scanf("%f", &OP2);
+       printf("Choose Operation ( + - * / ):");
+       fflush(stdin);
+       scanf("%c", &caracter);
+       
+       switch (caracter){
+              case '+': printf("Sum result: %f", OP1+OP2); break;
+                   
+              case '-': printf("Subtraction result: %f", OP1-OP2); break;
+                   
+              case '*': printf("Multiplication result: %f", OP1*OP2); break;
+                 
+              case '/': if(OP2 != 0) printf("Division result: %f", OP1/OP2); break;
+               
+              default: printf("Invalid Operation");  
+       }
+       
+       getch();
+       
+       
+}
